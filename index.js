67ba46bab3f3a41d55eebc0b6fd5313ac5387ee0
trabnett <timrabnett@gmail.com>
2019-02-10 19:22:04 +0000

    this is a functioning soultion to the problem, but it is not passing the tests. I am going to talk to a mentor about this when I have the time

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 function sumItems(array) {
-  // Sum all the numbers in the array
+  if (typeof flag === "undefined") {
+    flag = 0;
+  }
+  for (let item in array) {
+    if (Array.isArray(array[item])) {
+      sumItems(array[item])
+    } else if (Number.isInteger(array[item])) {
+      flag =  flag + array[item]
+    }
+  }
+  return flag
 }
 
+let array = [[1, 2, [[3], 4]], 5, []]
+console.log(sumItems(array))
 module.exports = sumItems;
